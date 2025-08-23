fc54c20f96c413f448bf61c730aa70790b484c22
PyMarcus <marcus-v@outlook.com>
2025-08-23 11:36:27 -0300

    chore(deps): update go mod

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
 )
 
 type Config struct {
@@ -12,6 +14,16 @@ type Config struct {
 	ErrorLog *log.Logger
 }
 
-func main() {
+var myApp Config
 
+func main() {
+	fyneApp := app.NewWithID("ca.gocode.goldwatcher.preferences")
+	myApp.App = fyneApp
+
+	myApp.InfoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	myApp.ErrorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	win := fyneApp.NewWindow("Monitor do Ouro")
+
+	win.ShowAndRun()
 }
